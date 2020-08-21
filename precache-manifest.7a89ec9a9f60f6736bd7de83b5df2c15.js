97a6a038a96915d27346cc98616cddfaf080170c
mmqn <michaelng728@gmail.com>
2020-08-20 22:05:44 -0700

    Updates

---
+++ precache-manifest.7a89ec9a9f60f6736bd7de83b5df2c15.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e67af3056e7a334e72cd393cf047101",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "776d764ead3a979a9eef",
+    "url": "/portfolio/static/css/main.da1914b0.chunk.css"
+  },
+  {
+    "revision": "55bcafb48db82bd8a35f",
+    "url": "/portfolio/static/js/2.6c885814.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.6c885814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776d764ead3a979a9eef",
+    "url": "/portfolio/static/js/main.f10c6410.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "955bd039850014720c4e6f5f67e0ca91",
+    "url": "/portfolio/static/media/IBMPlexMono-Regular.955bd039.otf"
+  },
+  {
+    "revision": "749823864c923056a30ec5c89bb40119",
+    "url": "/portfolio/static/media/IBMPlexSans-Medium.74982386.otf"
+  },
+  {
+    "revision": "177a43ac4fc0a37d2a513f485415df99",
+    "url": "/portfolio/static/media/IBMPlexSans-Regular.177a43ac.otf"
+  },
+  {
+    "revision": "288e2aae20ca47094ba09cbb5fc559bc",
+    "url": "/portfolio/static/media/chevron_down.288e2aae.svg"
+  },
+  {
+    "revision": "e9ae414a8747842da772ae4beed624e9",
+    "url": "/portfolio/static/media/logo_dark.e9ae414a.svg"
+  },
+  {
+    "revision": "ebed6ab3cb10333fd8dd40389329be56",
+    "url": "/portfolio/static/media/logo_light.ebed6ab3.svg"
+  }
+]);
