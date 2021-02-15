7f2250f4cf46c7b7f49c2971f5e48d4800191323
Michael M. Q. Nguyen <32374046+mmqn@users.noreply.github.com>
2021-02-15 10:22:50 -0800

    Updated URL endpoint for story Markdowns

--- src/util/setMarkdownContent.js
+++ src/util/setMarkdownContent.js
@@ -2,7 +2,7 @@ import convertMarkdown from 'marked';
 
 export default async ({ storyId, setContent }) => {
   try {
-    const contentPath = `https://raw.githubusercontent.com/mmqn/portfolio_stories/master/${storyId}/${storyId}.md`;
+    const contentPath = `https://raw.githubusercontent.com/mmqn/portfolio-stories/master/${storyId}/${storyId}.md`;
 
     const response = await fetch(contentPath);
     const content = await response.text();
